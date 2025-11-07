d7cdab6eba88aa9fd531c52b30343f1ab20ccc83
klinx01 <vadimkorotnan@gmail.com>
2025-11-07 15:24:17 +0200

    fix error

--- homework-5.js
+++ homework-5.js
@@ -1,63 +1,49 @@
 // Задание №3, вывести  в консоль нововости
 
-const city = "Киевe"
-
-function sayCity() {
-  console.log(city)
-};
-
-
-let temperature = "17\u00B0"
-
-function sayCityTemperature() {
-  console.log(temperature)
-};
-
-
-function news() {
+function news(city, temperature) {
   console.log(`Сейчас в ${city} температура ${temperature} —  градусов по Цельсию`)
 };
 
-news()
+news("Киевe", "17\u00B0")
 
 //Задание №4, сравнить скорость звука с помощью if else
 
-const speedOfSound = 343
+const SPEED_OF_SOUND = 343
 
-function speed(currentSpeed) {
-  if (currentSpeed > speedOfSound) {
+function calculateSpeed(currentSpeed) {
+  if (currentSpeed > SPEED_OF_SOUND) {
     console.log("Сверхзвуковая скорость")
   } else {
     console.log("Дозвуковая скорость")
   }
 };
 
-speed(394)
+calculateSpeed(394)
 
 // Задание №5, высчитать разницу
 
-const chocopay = "Чокопай"
-const priceChokopay = 289
+const productChocopay = "Чокопай"
+const CHOCOPAY_PRODUCT_PRICE = 289
 
-function checkBudget(currentBudget) {
-  if (currentBudget > priceChokopay) {
-    console.log("Ваш Чокопай приобретён. Спасибо за покупку!")
+function buyProductChocopay(currentBudget) {
+  if (currentBudget > CHOCOPAY_PRODUCT_PRICE) {
+    console.log(`Ваш ${productChocopay} приобретён. Спасибо за покупку!`)
   } else {
-    const difference = priceChokopay - currentBudget
+    const difference = CHOCOPAY_PRODUCT_PRICE - currentBudget
     console.log(`Вам не хватает ${difference}$, пополните баланс`)
   }
 };
 
-checkBudget(203)
-checkBudget(411)
+buyProductChocopay(203)
+buyProductChocopay(411)
 
 // Задание №6, создавть 1 функцию
 
-function sayMessage() {
+function displayMessage() {
   console.log("я не знаю что можно придумать")
 }
 
-sayMessage()
+displayMessage()
 
 // Задание №7, создать 3 переменные
 
